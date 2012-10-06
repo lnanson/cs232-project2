f7a2658bfc17c4d03d90957baaeec4a1779e55a2
Lynde Nanson <lnanson@jsu.edu>
2012-10-06 13:19:41 -0500

    added the basic components for the main window

--- Project2.java
+++ Project2.java
@@ -3,7 +3,14 @@ public class Project2 {
 
 
 	public static void main(String[] args) {
-
+		JFrame frame = new JFrame("Create Ascii Image");
+		frame.add(ImageViewer);		
+		frame.add(AsciiViewer);
+		JScrollPane scrollImage = new JScrollPane(ImageViewer);
+		JScrollPane scrollAscii = new JScrollPane(AsciiViewer);
+		add(scrollImage);
+		add(scrollAscii);
+		frame.add(ZoomControl);
 
 
 	}
